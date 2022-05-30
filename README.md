f4fde90de00a354cb903928eb144fa42db3893ff
luowei0123456789 <1185916072@qq.com>
2022-05-30 14:07:42 +0800

    new: 初始化项目

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # leetcode
 力扣算法题练习
+
+##文件目录结构
+###1.offer——剑指 Offer（专项突击版）算法题库   
+####1.1.地址：
+https://leetcode.cn/problemset/all/?listId=e8X3pBZi&page=1
+
+####1.2.test1.java： 
+```
+剑指 Offer II 001. 整数除法
+剑指 Offer II 002. 二进制加法
+剑指 Offer II 003. 前 n 个数字二进制中 1 的个数
+剑指 Offer II 004. 只出现一次的数字
+剑指 Offer II 005. 单词长度的最大乘积
+剑指 Offer II 006. 排序数组中两个数字之和
+剑指 Offer II 007. 数组中和为 0 的三个数
+剑指 Offer II 008. 和大于等于 target 的最短子数组
+剑指 Offer II 009. 乘积小于 K 的子数组
+剑指 Offer II 010. 和为 k 的子数组
+``` 
+   
---
+++ offer/src/test1.java
@@ -0,0 +1,50 @@
+package src;
+
+/**
+ * @author: luow
+ * @date: 2022/5/30 10:28
+ * @describe TODO
+ */
+public class test1 {
+
+    public static void main(String[] args) {
+//        test01();
+    }
+
+    /**
+     * @Author luow
+     * @Date 11:33 2022/5/30
+     * @describe 整数除法
+     *
+     * 给定两个整数 a 和 b ，求它们的除法的商 a/b ，要求不得使用乘号 '*'、除号 '/' 以及求余符号 '%' 。
+     *
+     * 示例 1：
+     *
+     * 输入：a = 15, b = 2
+     * 输出：7
+     * 解释：15/2 = truncate(7.5) = 7
+     * 示例 2：
+     *
+     * 输入：a = 7, b = -3
+     * 输出：-2
+     * 解释：7/-3 = truncate(-2.33333..) = -2
+     * 示例 3：
+     *
+     * 输入：a = 0, b = 1
+     * 输出：0
+     * 示例 4：
+     *
+     * 输入：a = 1, b = 1
+     * 输出：1
+     *
+     */
+    public static void test01(){}
+
+    public static void test02(){}
+
+    public static void test03(){}
+
+    public static void test04(){}
+
+    public static void test05(){}
+}
